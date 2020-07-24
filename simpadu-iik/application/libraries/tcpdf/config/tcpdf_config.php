5c4d6883969db79b384d8ba75a06f8c53b78a01d
dzulfikriAlfik <fikriallfik@gmail.com>
2020-07-24 08:36:33 +0700

    edit default images header for tcpdf config

--- simpadu-iik/application/libraries/tcpdf/config/tcpdf_config.php
+++ simpadu-iik/application/libraries/tcpdf/config/tcpdf_config.php
@@ -63,13 +63,13 @@
  * Default images directory.
  * By default it is automatically set but you can also set it as a fixed string to improve performances.
  */
-//define ('K_PATH_IMAGES', '');
+define ('K_PATH_IMAGES', dirname(__FILE__).'/../images/');
 
 /**
  * Deafult image logo used be the default Header() method.
  * Please set here your own logo or an empty string to disable it.
  */
-//define ('PDF_HEADER_LOGO', '');
+define ('PDF_HEADER_LOGO', 'tcpdf_logo.jpg');
 
 /**
  * Header logo image width in user units.
