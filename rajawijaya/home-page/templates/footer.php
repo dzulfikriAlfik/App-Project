e6744177abafa4f6ec0675d20865d433d7c920cb
Dzulfikri Alkautsari <dzulfikrialfik@gmail.com>
2021-10-13 16:50:38 +0700

    Initial commit project

---
+++ rajawijaya/home-page/templates/footer.php
@@ -0,0 +1,117 @@
+</div>
+
+<!-- Begin footer -->
+<footer class="footer">
+   <div class="footer-main">
+      <div class="container">
+         <div class="row items">
+            <div class="col-xl-3 col-lg-3 col-md-5 col-12 item">
+               <!-- Begin company info -->
+               <div class="footer-company-info">
+                  <div class="footer-company-top">
+                     <a href="/" class="logo" title="PathSoft">
+                        <img data-src="home-page/assets/img/logo-white.svg" width="115" height="36" class="lazy" src="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" alt="PathSoft">
+                     </a>
+                     <div class="footer-company-desc">
+                        <p>Our company has been developing high-quality and reliable software for corporate needs since 2008. We are renowned professionals of software development.</p>
+                     </div>
+                  </div>
+                  <ul class="footer-social-links">
+                     <li>
+                        <a href="#!" title="Facebook">
+                           <svg viewBox="0 0 320 512">
+                              <use xlink:href="home-page/assets/img/sprite.svg#facebook-icon"></use>
+                           </svg>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="#!" title="Instagram">
+                           <svg viewBox="0 0 448 512">
+                              <use xlink:href="home-page/assets/img/sprite.svg#instagram-icon"></use>
+                           </svg>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="#!" title="LinkedIn">
+                           <svg viewBox="0 0 448 512">
+                              <use xlink:href="home-page/assets/img/sprite.svg#linkedin-icon"></use>
+                           </svg>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="#!" title="Twitter">
+                           <svg viewBox="0 0 512 512">
+                              <use xlink:href="home-page/assets/img/sprite.svg#twitter-icon"></use>
+                           </svg>
+                        </a>
+                     </li>
+                  </ul>
+               </div>
+               <!-- End company info -->
+            </div>
+            <div class="col-xl-2 offset-xl-1 col-md-3 col-5 col-lg-2 item">
+               <div class="footer-item">
+                  <p class="footer-item-heading">Menu</p>
+                  <nav class="footer-nav">
+                     <ul class="footer-mnu">
+                        <li><a href="about-us.html" class="hover-link" data-title="About Us"><span>About Us</span></a></li>
+                        <li><a href="services.html" class="hover-link" data-title="Services"><span>Services</span></a></li>
+                        <li><a href="typography.html" data-title="Typography"><span>Typography</span></a></li>
+                        <li><a href="news.html" class="hover-link" data-title="News"><span>News</span></a></li>
+                        <li><a href="contact-us.html" class="hover-link" data-title="Contact Us"><span>Contact Us</span></a></li>
+                     </ul>
+                  </nav>
+               </div>
+            </div>
+            <div class="col-xs-4 col-lg-4 col-12 item">
+               <div class="footer-item">
+                  <p class="footer-item-heading">Our contacts</p>
+                  <ul class="footer-contacts">
+                     <li>
+                        <i class="material-icons md-22">location_on</i>
+                        <div class="footer-contact-info">
+                           301 S Irving Blvd Los Angeles, CA 90020
+                        </div>
+                     </li>
+                     <li>
+                        <i class="material-icons md-22 footer-contact-tel">smartphone</i>
+                        <div class="footer-contact-info">
+                           <a href="#!" class="formingHrefTel">+1 323-913-4688</a>, <a href="#!" class="formingHrefTel">+1 323-888-4554</a>
+                        </div>
+                     </li>
+                     <li>
+                        <i class="material-icons md-22 footer-contact-email">email</i>
+                        <div class="footer-contact-info">
+                           <a href="mailto:mail@example.com">mail@example.com</a>
+                        </div>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="footer-bottom">
+      <div class="container">
+         <div class="row justify-content-center items">
+            <div class="col-md-auto col-12 item">
+               <div class="copyright">© 2021 Raja Wijaya</div>
+            </div>
+         </div>
+      </div>
+   </div>
+</footer><!-- End footer -->
+
+</main><!-- End main -->
+
+<script src="home-page/assets/libs/jquery/jquery.min.js"></script>
+<script src="home-page/assets/libs/lozad/lozad.min.js"></script>
+<script src="home-page/assets/libs/device/device.js"></script>
+<script src="home-page/assets/libs/ScrollToFixed/jquery-scrolltofixed-min.js"></script>
+<script src="home-page/assets/libs/spincrement/jquery.spincrement.min.js"></script>
+<script src="home-page/assets/libs/jquery-validation-1.19.3/jquery.validate.min.js"></script>
+<script src="home-page/assets/js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ rajawijaya/home-page/templates/header.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+   <meta charset="utf-8">
+
+   <title>Raja Wijaya Cirebon</title>
+
+   <meta name="description" content="Description">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2">
+
+   <link rel="icon" href="home-page/assets/img/favicon/favicon.ico" type="image/x-icon">
+
+   <link rel="stylesheet" href="home-page/assets/css/libs.css">
+   <link rel="stylesheet" href="home-page/assets/css/style.css">
+   <link rel="preload" href="home-page/assets/fonts/istok-web-v15-latin/istok-web-v15-latin-regular.woff2" as="font" type="font/woff2" crossorigin>
+   <link rel="preload" href="home-page/assets/fonts/istok-web-v15-latin/istok-web-v15-latin-700.woff2" as="font" type="font/woff2" crossorigin>
+   <link rel="preload" href="home-page/assets/fonts/montserrat-v15-latin/montserrat-v15-latin-700.woff2" as="font" type="font/woff2" crossorigin>
+   <link rel="preload" href="home-page/assets/fonts/montserrat-v15-latin/montserrat-v15-latin-600.woff2" as="font" type="font/woff2" crossorigin>
+
+   <link rel="preload" href="home-page/assets/fonts/material-icons/material-icons.woff2" as="font" type="font/woff2" crossorigin>
+   <link rel="preload" href="home-page/assets/fonts/material-icons/material-icons-outlined.woff2" as="font" type="font/woff2" crossorigin>
+   <!-- myCustom CSS -->
+   <link rel="stylesheet" href="home-page/assets/css/custom.css">
+
+</head>
+
+
+<body>
+
+   <main class="main">
+
+      <div class="main-inner">
+
+         <!-- Begin mobile main menu -->
+         <nav class="mmm">
+            <div class="mmm-content">
+               <ul class="mmm-list">
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="about-us.html">About us</a></li>
+                  <li><a href="services.html">Services</a></li>
+                  <li><a href="typography.html">Typography</a></li>
+                  <li><a href="tabs-and-accordions.html">Tabs & Accordions</a></li>
+                  <li><a href="news.html">News</a></li>
+                  <li><a href="contact-us.html">Contact Us</a></li>
+               </ul>
+            </div>
+         </nav><!-- End mobile main menu -->
+
+         <!-- Begin header -->
+         <header class="header">
+            <!-- Begin header top -->
+            <nav class="header-top">
+               <div class="container">
+                  <div class="row align-items-center justify-content-between">
+                     <div class="col-auto">
+                        <!-- Begin header top info -->
+                        <ul class="header-top-info">
+                           <li>
+                              <a href="mailto:mail@example.com">
+                                 <i class="material-icons md-18">mail_outline</i>
+                                 <span>mail@example.com</span>
+                              </a>
+                           </li>
+                           <li>
+                              <a href="mailto:mail@example.com" class="formingHrefTel">
+                                 <i class="material-icons md-18">phone_in_talk</i>
+                                 <span>+1-888-777-1234</span>
+                              </a>
+                           </li>
+                        </ul><!-- Ennd header top info -->
+                     </div>
+                     <div class="col-auto">
+                        <div class="header-top-links">
+                           <!-- Begin social links -->
+                           <ul class="social-links">
+                              <li>
+                                 <a href="#!" title="Facebook">
+                                    <svg viewBox="0 0 320 512">
+                                       <use xlink:href="home-page/assets/img/sprite.svg#facebook-icon"></use>
+                                    </svg>
+                                 </a>
+                              </li>
+                              <li>
+                                 <a href="#!" title="Instagram">
+                                    <svg viewBox="0 0 448 512">
+                                       <use xlink:href="home-page/assets/img/sprite.svg#instagram-icon"></use>
+                                    </svg>
+                                 </a>
+                              </li>
+                              <li>
+                                 <a href="#!" title="LinkedIn">
+                                    <svg viewBox="0 0 448 512">
+                                       <use xlink:href="home-page/assets/img/sprite.svg#linkedin-icon"></use>
+                                    </svg>
+                                 </a>
+                              </li>
+                              <li>
+                                 <a href="#!" title="Twitter">
+                                    <svg viewBox="0 0 512 512">
+                                       <use xlink:href="home-page/assets/img/sprite.svg#twitter-icon"></use>
+                                    </svg>
+                                 </a>
+                              </li>
+                           </ul><!-- End social links -->
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </nav><!-- End header top -->
+
+            <!-- Begin header fixed -->
+            <nav class="header-fixed">
+               <div class="container">
+                  <div class="row flex-nowrap align-items-center justify-content-between">
+                     <div class="col-auto d-block d-lg-none header-fixed-col">
+                        <div class="main-mnu-btn">
+                           <span class="bar bar-1"></span>
+                           <span class="bar bar-2"></span>
+                           <span class="bar bar-3"></span>
+                           <span class="bar bar-4"></span>
+                        </div>
+                     </div>
+                     <div class="col-auto header-fixed-col">
+                        <!-- Begin logo -->
+                        <a href="/" class="logo" title="PathSoft">
+                           <img src="home-page/assets/img/logo.svg" width="115" height="36" alt="PathSoft">
+                        </a><!-- End logo -->
+                     </div>
+                     <div class="col-auto header-fixed-col d-none d-lg-block col-static">
+                        <!-- Begin main menu -->
+                        <nav class="main-mnu">
+                           <ul class="main-mnu-list">
+                              <li>
+                                 <a href="#" data-title="Home"><span>Home</span></a>
+                              </li>
+                              <li>
+                                 <a href="#" data-title="Tentang Kami"><span>Tentang Kami</span></a>
+                              </li>
+                              <li>
+                                 <a href="admin-page/auth/login.php" data-title="Login/Daftar"><span>Login/Daftar</span></a>
+                              </li>
+                           </ul>
+                        </nav><!-- End main menu -->
+                     </div>
+                     <div class="col-auto header-fixed-col col-static">
+                        <!-- Begin header actions -->
+                        <ul class="header-actions">
+                           <!-- Begin header navbar -->
+                           <li class="d-block d-lg-none">
+                              <div class="header-navbar">
+                                 <div class="header-navbar-btn">
+                                    <i class="material-icons md-24">more_vert</i>
+                                 </div>
+                                 <ul class="header-navbar-content">
+                                    <li>
+                                       <a href="mailto:mail@example.com">
+                                          <i class="material-icons md-20">mail_outline</i>
+                                          <span>mail@example.com</span>
+                                       </a>
+                                    </li>
+                                    <li>
+                                       <a href="mailto:mail@example.com" class="formingHrefTel">
+                                          <i class="material-icons md-20">support_agent</i>
+                                          <span>1-888-777-1234</span>
+                                       </a>
+                                    </li>
+                                    <li>
+                                       <!-- Begin social links -->
+                                       <ul class="social-links">
+                                          <li>
+                                             <a href="#!" title="Facebook">
+                                                <svg viewBox="0 0 320 512">
+                                                   <use xlink:href="home-page/assets/img/sprite.svg#facebook-icon"></use>
+                                                </svg>
+                                             </a>
+                                          </li>
+                                          <li>
+                                             <a href="#!" title="Instagram">
+                                                <svg viewBox="0 0 448 512">
+                                                   <use xlink:href="home-page/assets/img/sprite.svg#instagram-icon"></use>
+                                                </svg>
+                                             </a>
+                                          </li>
+                                          <li>
+                                             <a href="#!" title="LinkedIn">
+                                                <svg viewBox="0 0 448 512">
+                                                   <use xlink:href="home-page/assets/img/sprite.svg#linkedin-icon"></use>
+                                                </svg>
+                                             </a>
+                                          </li>
+                                          <li>
+                                             <a href="#!" title="Twitter">
+                                                <svg viewBox="0 0 512 512">
+                                                   <use xlink:href="home-page/assets/img/sprite.svg#twitter-icon"></use>
+                                                </svg>
+                                             </a>
+                                          </li>
+                                       </ul><!-- End social links -->
+                                    </li>
+                                 </ul>
+                              </div>
+                           </li><!-- End header navbar -->
+                        </ul><!-- End header actions -->
+                     </div>
+                  </div>
+               </div>
+            </nav><!-- End header fixed -->
+         </header><!-- End header -->
